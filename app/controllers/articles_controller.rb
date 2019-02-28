a61619960bb08c257ce183ffcb87129d2420989d
gngo2018 <gngo2018@gmail.com>
2019-02-28 14:20:45 -0500

    ActiveRecord Querying

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,7 +42,6 @@ class ArticlesController < ApplicationController
         redirect_to articles_path
     end
 
-
     private
         def article_params
             params.require(:article).permit(:title, :text)
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,10 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def sql_query
+
+    end
+    
 private
     def comment_params
         params.require(:comment).permit(:commenter, :body)
